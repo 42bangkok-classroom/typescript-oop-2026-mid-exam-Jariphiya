c202d01dcad27992a17764537d8bc3aff78591ef
Jariphiya <68010130@kmtil.ac.th>
2026-01-21 16:11:07 +0700

    update?

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,9 +1,19 @@
 
-const n =Number(process.argv[2]);
-if (!isNaN(n) && n >0) {
-let row ="";
-for (let i =1; i <= n; i++) {
-    row += i;
-for (let i =0; i < n; i++)console.log(row);}
+let n = Number(process.argv[2])
+
+if(isNaN(n) || n <= 0){
+    process.exit()
 }
 
+let re = ""
+for(let row = 1; row <= n; row++){
+    for(let col = 0; col < n-row; col++){
+        re += " "
+    }
+    for(let coll = 0; coll < n; coll ++){
+        re += row
+    }
+
+    re += "\n"
+}
+console.log(re)
--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,16 @@
 const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
-  // Write your code below
+  let output = ""
+
+  for(let index = 0; index < comment.length; index ++ ){
+  if(comment[index].length >= 5 && comment[index].length <= 10){
+    output += comment[index]
+  }
+  output += " "
 }
+
+  return output
+}
+
+console.log(shortenComment(comment))
